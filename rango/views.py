1b38bf2506730f9cdd5c1a71b7473bd31fa7449b
toanhtran <toanh_tran@yahoo.com>
2015-11-25 20:28:42 +0900

    add rango urls.py

---
+++ rango/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("Rango says hey there world!")
+	
+def about(request):
+	return HttpResponse("Rango says here is the about page.")
+
