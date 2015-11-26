d163155df6d6a2b0c92c75c139d5efb476b19bc4
toanhtran <toanh_tran@yahoo.com>
2015-11-27 08:39:43 +0900

    config admin interface

--- rango/views.py
+++ rango/views.py
@@ -3,11 +3,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    context_dict = {'boldmessage': "Full Stack Developer for PowerToFly!"}
+    context_dict = {'boldmessage': "handsome man!"}
     return render(request, 'rango/index.html', context_dict)
 
 
 def about(request):
-	fun_facts = {'italicmessage': "This is my year! Anything I put my mind to I accomplish."}
+	fun_facts = {'italicmessage': "the best dad ever! He should get father of the year!"}
 	return render(request, 'rango/about.html', fun_facts)
 
