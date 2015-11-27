2102285dd9f3d9e13e9a83b72b4a4a7e7c0058d0
toanhtran <toanh_tran@yahoo.com>
2015-11-27 09:11:08 +0900

    add population script

--- rango/models.py
+++ rango/models.py
@@ -10,7 +10,7 @@ class Page(models.Model):
 	category = models.ForeignKey(Category)
 	title = models.CharField(max_length=128)
 	url = models.URLField()
-	views = models.IntegerField(default =0)
+	views = models.IntegerField(default=0)
 
 	def __str__(self):
 		return self.title
