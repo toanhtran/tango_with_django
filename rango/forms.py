2f87730b00a6dc5a1548a319e6652a0f86251f87
toanhtran <toanh_tran@yahoo.com>
2015-11-28 14:40:36 +0900

    add add_category field/ form

--- rango/forms.py
+++ rango/forms.py
@@ -1,23 +1,36 @@
 from django import forms
-form rango.models import Page, Category
+from rango.models import Page, Category
 
-class CategoryFrom(forms.ModelForm):
-	name = forms.CharField(max_length=128, help_text="Please enter the category name.")
-	views = form.IntegerField(widget=forms.HiddenInput(), initial=0)
-	likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-	slug = forms.CharField(widget=forms.HiddenInput()),
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
-	class Meta:
-		model = Category
-		fields = ('name',)
+    # An inline class to provide additional information on the form.
+    class Meta:
+        # Provide an association between the ModelForm and a model
+        model = Category
+        fields = ('name',)
+
 
 class PageForm(forms.ModelForm):
-	title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
-	url = forms.URLField(max_length=200,help_text="Please enter the URL of the page.")
-	views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
-	class Meta:
-		model = Page
+    class Meta:
+        # Provide an association between the ModelForm and a model
+        model = Page
+        exclude = ('category',)
+        
+        def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
 
-		exclude = ('category')
+        # If url is not empty and doesn't start with 'http://', prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
 
+        return cleaned_data
--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 
 def index(request):
 	category_list = Category.objects.order_by('-likes')
@@ -34,3 +35,41 @@ def category(request, category_name_slug):
 
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context_dict)
+
+
+def add_category(request):
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = CategoryForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render(request, 'rango/add_category.html', {'form': form})
+
+
+
+
+
+
+
+
+
+
+
+
+
